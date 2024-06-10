463d787d9a5c36ac824b69724d15e7c668be539f
Setupp <csmbcarlos@gmail.com>
2024-06-10 18:24:34 -0400

    Agregado inicio del push HM y arreglado xml

--- MyApplication4/app/src/main/java/com/example/myapplication/HorasMedicas.kt
+++ MyApplication4/app/src/main/java/com/example/myapplication/HorasMedicas.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.icu.text.DateFormat.HourCycle
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
@@ -7,6 +9,9 @@ import android.widget.CalendarView
 import android.widget.EditText
 import android.widget.TimePicker
 import androidx.appcompat.app.AlertDialog
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.Date
 
 class HorasMedicas : AppCompatActivity() {
@@ -20,10 +25,11 @@ class HorasMedicas : AppCompatActivity() {
             val FechaMillis = findViewById<CalendarView>(R.id.calendarView).date
             val Fecha = Date(FechaMillis)
             val Hora = findViewById<TimePicker>(R.id.timePicker).hour
+            val Minuto = findViewById<TimePicker>(R.id.timePicker).minute
 
             if (Tratamiento.isNotEmpty()) {
                 // Agregar la hora médica a la base de datos
-                TODO()
+                cargarRecordatorio(Tratamiento, Fecha, Hora, Minuto)
             } else {
                 mostrarAlertDialog("Error", "Debe completar el campo de Nombre del tratamiento.") {}
             }
@@ -44,5 +50,18 @@ class HorasMedicas : AppCompatActivity() {
                 // Acción al hacer clic en Aceptar en el AlertDialog
             }
             .show()
+    }
+
+    private fun cargarRecordatorio(String tratamiento: String, Date fecha: Date, Int hora: Int, Int minuto: Int) {
+        // Cargar el recordatorio a la base de datos
+        // Obtener una instancia de SharedPreferences
+        val sharedPref = this.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+        // Obtener el USER_ID de SharedPreferences
+        val id = sharedPref.getString("USER_ID", null)
+
+        // Guardar el medicamento
+        CoroutineScope(Dispatchers.IO).launch{
+            // TODO: Implementar la carga del recordatorio
+        }
     }
 }
