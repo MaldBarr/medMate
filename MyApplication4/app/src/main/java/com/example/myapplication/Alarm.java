2479c287476a6d26b9ff043a06f4cd248bb9b964
Alvaro <valdivialvaro.8@gmail.com>
2024-06-11 20:35:41 -0400

    alarma sonando sin notificación

---
+++ MyApplication4/app/src/main/java/com/example/myapplication/Alarm.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+
+public class Alarm extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MediaPlayer mp=MediaPlayer.create(context,Settings.System.DEFAULT_ALARM_ALERT_URI);
+        mp.start();
+    }
+}
